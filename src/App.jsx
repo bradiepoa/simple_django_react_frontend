f98809a4a0e11a70cc97b0ab6664bfb96f035a09
bradiepoa7@gmail.com <bradiepoa7@gmail.com>
2024-06-15 18:10:52 +0300

    importing all components in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
 import react from "react"
+import { BrowserRouter, Routes,Route,Navigate } from "react-router-dom"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
+import protectedRoute from "./components"
+
 function App() {
 
   return (
