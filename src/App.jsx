49819becfda8b331a79986a95a76682bc09dfc9a
bradiepoa7@gmail.com <bradiepoa7@gmail.com>
2024-06-16 10:20:11 +0300

    added logout function and navigation functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,36 @@ import Login from "./pages/Login"
 import Register from "./pages/Register"
 import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
-import protectedRoute from "./components"
+import protectedRoute from "./components/ProtectedRoute"
+
+function Logout() {
+  localStorage.clear()
+  return <Navigate to="/Login" />
+}
+
+function RegisterAndLogout(){
+  localStorage.clear()
+  return <Register />
+}
 
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={
+        <protectedRoute>
+          <Home />
+        </protectedRoute>
+      } 
+      />
+      <Route path="/login" element={<Login /> } />
+      <Route path="/register" element={<RegisterAndLogout /> } />
+      <Route path="*" element={<NotFound /> } />
+    </Routes>
+    </BrowserRouter>
      
-    </>
+   
   )
 }
-
 export default App
