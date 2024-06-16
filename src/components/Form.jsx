0d3d1685f9085ca333b348b30c6dee0177b95365
bradiepoa7@gmail.com <bradiepoa7@gmail.com>
2024-06-16 11:39:52 +0300

    adding css files

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import api from "../api"
 import { useNavigate } from "react-router-dom"
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants"
+import "..styles/Form.css"
 
 function Form({route, method}) {
     const [username, setUsername] = useState("")
@@ -11,9 +12,26 @@ function Form({route, method}) {
 
     const name = method === "login" ? "Login" : "Register"
 
-    const handleSubmit = (e) =>{
-        e.preventDefault()
-    }
+    const handleSubmit = async (e) =>{
+        setLoading(true);
+        e.preventDefault();
+
+        try { 
+            const res = await api.post(route, { username, password })
+            if (method === "login"){
+                localStorage.setItem(ACCESS_TOKEN, res.data.access);
+                localStorage.setItem(REFRESH_TOKEN, res.data.refresh)
+                navigate("/")
+            }else{
+                navigate("/login")
+            }
+        }
+        catch (error){
+            alert(error)
+        } finally {
+            setLoading(false)
+        }
+    };
 
     return <form onSubmit={handleSubmit} className="form-container">
         <h1>{ name }</h1>
